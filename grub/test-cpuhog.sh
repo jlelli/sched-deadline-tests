851f85b7a6b9958fdd5d32b651243acf015816df
Juri Lelli <juri.lelli@arm.com>
2014-09-12 09:21:05 +0100

    import current status

---
+++ grub/test-cpuhog.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+. ../utils.sh
+
+TNAME=$(echo $0 | tr '/' ' ' | tr '.' ' ' | awk '{ print $1 }')
+TINFO="Test how a cpuhog task is handled by GRUB"
+BENCHMARK="rt-app"
+TRACE=$1
+EVENTS="sched_wakeup* sched_switch sched_migrate* sched_stat_running_bw* sched_stat_*_dl"
+
+cleanup() {
+  trace_stop
+  turn_on_cpu 1
+  turn_on_cpu 2
+  turn_on_cpu 3
+  turn_on_cpu 4
+
+  for c in ${ALL_MASK}; do
+    set_cpufreq ${c} ondemand
+  done
+
+  exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+print_test_info
+
+trace_start
+#disable_ac
+turn_off_cpu 1
+turn_off_cpu 2
+turn_off_cpu 3
+turn_off_cpu 4
+
+TASKSET="${TNAME}.json"
+
+#for c in ${A15_MASK}; do
+#  # this corresponds to 1024 cap on A15
+#  set_cpufreq ${c} userspace 1200000
+#done
+set_cpufreq 0 userspace 1200000
+
+log "waiting 5 seconds..."
+sleep 5
+log "running taskset ${TASKSET} (at 1024 cap)"
+mkdir -p ./log
+${BENCHMARK} ${TASKSET} >> ${TNAME}.out 2>&1
+
+log "${TNAME} execution finished"
+
+trace_stop
+trace_extract
+
+cleanup
---
+++ utils.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+A7_FREQS="350000 400000 500000 600000 700000 800000 900000 1000000"
+A15_FREQS="500000 600000 700000 800000 900000 1000000 1100000 1200000"
+ALL_MASK="0 1 2 3 4"
+A7_MASK="2 3 4"
+A15_MASK="0 1"
+ONE_A7_ONE_A15_NMASK="2 3 4"
+TRACE_CMD="/usr/local/bin/trace-cmd"
+tracing=0
+rt_runtime=-1
+
+print_test_info() {
+  echo "************************************************"
+  echo "* ${TNAME}"
+  echo "*"
+  echo "* ${TINFO}"
+  echo "*"
+  echo "* Clusters: A7 [${A7_MASK}] A15 [${A15_MASK}]"
+  echo "*"
+  echo "************************************************"
+  echo
+}
+
+trace_start() {
+  if [ ${TRACE} -eq 1 ]; then
+    tracing=1
+    events=""
+    for e in ${EVENTS}; do
+      events+="-e ${e} "
+    done
+    ${TRACE_CMD} start ${events} >${TNAME}.out 2>&1
+    echo "tracing started"
+  fi
+}
+
+trace_stop() {
+  if [ ${tracing} -eq 1 ]; then
+    ${TRACE_CMD} stop >>${TNAME}.out 2>&1
+    echo "tracing stopped"
+  fi
+}
+
+trace_extract() {
+  if [ ${tracing} -eq 1 ]; then
+    ${TRACE_CMD} extract -o trace-${TNAME}.dat >>${TNAME}.out 2>&1
+  fi
+
+  tracing=0
+}
+
+trace_write() {
+  if [ ${tracing} -eq 1 ]; then
+    echo $1 > /sys/kernel/debug/tracing/trace_marker
+  fi
+}
+
+test_failed() {
+  echo "TEST_FAILED"
+  trace_write "TEST_FAILED"
+  trace_stop
+  trace_extract
+
+  exit 1
+}
+
+test_passed() {
+  echo "TEST_PASSED"
+  trace_write "TEST_PASSED"
+}
+
+disable_ac() {
+  echo "disabling admission control"
+  rt_runtime=$(cat /proc/sys/kernel/sched_rt_runtime_us)
+  echo -1 > /proc/sys/kernel/sched_rt_runtime_us
+}
+
+enable_ac() {
+  echo "enabling admission control"
+  echo ${rt_runtime} > /proc/sys/kernel/sched_rt_runtime_us
+}
+
+set_cpufreq() {
+  if [ -z "$3" ]; then
+    echo "setting CPU$1 to $2 governor"
+    trace_write "setting CPU$1 to $2 governor"
+  else
+    echo "setting CPU$1 to $2 governor at $3 MHz"
+    trace_write "setting CPU$1 to $2 governor at $3 MHz"
+  fi
+
+  echo $2 > /sys/devices/system/cpu/cpu$1/cpufreq/scaling_governor
+
+  if [ ! -z "$3" ]; then
+    echo $3 > /sys/devices/system/cpu/cpu$1/cpufreq/scaling_setspeed
+  fi
+}
+
+set_runtime_freq_inv() {
+  echo "enabling runtime frequency invariance"
+  #echo DL_RUNTIME_FI > /sys/kernel/debug/sched_features
+  echo ENERGY_AWARE > /sys/kernel/debug/sched_features
+}
+
+clear_runtime_freq_inv() {
+  echo "disabling runtime frequency invariance"
+  #echo NO_DL_RUNTIME_FI > /sys/kernel/debug/sched_features
+  echo NO_ENERGY_AWARE > /sys/kernel/debug/sched_features
+}
+
+turn_off_cpu() {
+  echo "turning off CPU$1"
+  trace_write "turning off CPU$1"
+  echo 0 > /sys/devices/system/cpu/cpu$1/online
+}
+
+turn_on_cpu() {
+  echo "turning on CPU$1"
+  trace_write "turning on CPU$1"
+  echo 1 > /sys/devices/system/cpu/cpu$1/online
+}
+
+log() {
+  echo $1
+  trace_write $1
+}
