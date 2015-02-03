e8fefef4be780e41dd0817b97a10eb30c8d5514b
Juri Lelli <juri.lelli@arm.com>
2015-02-03 11:26:48 +0000

    basic: align test_yield_dl to test_skeleton

--- basic/test_yield_dl.sh
+++ basic/test_yield_dl.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 . ../utils.sh
+TFULL=`basename $0`
+TNAME=${TFULL%.*}
 TDESC="
 ##############################################
 #
+#    test: $TNAME
+#
 #    Stress SCHED_DEADLINE's yield semantic
+#    by running a task that perform a tight
+#    loop with yields for 5s.
 #
 ##############################################
 
 "
-TNAME="test_yield_dl"
 TRACE=${1-0}
 EVENTS="sched_wakeup* sched_switch sched_migrate*"
 
@@ -17,7 +22,9 @@ print_test_info
 dump_on_oops
 trace_start
 
+trace_write "start $TNAME"
 ./periodic_yield
+trace_write "end $TNAME"
 
 trace_stop
 trace_extract
