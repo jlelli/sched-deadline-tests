776414f4ed2c97a2f4a0437c80853d2a0e7d17cb
Juri Lelli <juri.lelli@gmail.com>
2015-07-15 08:08:59 +0100

    hotplug: fix test_hp_cpuset

--- hotplug/test_hp_cpuset.sh
+++ hotplug/test_hp_cpuset.sh
@@ -3,26 +3,52 @@
 TFULL=`basename $0`
 TNAME=${TFULL%.*}
 TDESC="
-###########################################
-#  
+###############################################################################
+#
 #    test: $TNAME
 #
-#    ...
+#    Create an exclusive cpuset composed of 3 CPUs and put a DL task to run
+#    into it. Start turning off CPUs and verify that CPUs can be turned off
+#    all but one (the last the task happens to run in).
 #
-###########################################
+###############################################################################
 
 "
 TRACE=${1-0}
-RUNS=5
+RUNS=${2-5}
 EVENTS="sched_wakeup* sched_switch sched_migrate*"
 CPUSET_DIR=/sys/fs/cgroup
 
 tear_down() {
   trace_write "kill $PID"
-  kill -9 $PID
+  kill -TERM $PID
   sleep 1
   rmdir ${CPUSET_DIR}/cpusetA
-  
+  if [ $? -ne 0 ]; then
+    trace_write "ERROR: failed to remove cpusetA"
+    exit 1
+  fi
+
+  sleep 1
+  trace_write "Moving tasks back in root cpuset: "
+  trace_write "tasks tasks: "
+  cat ${CPUSET_DIR}/cpuset-work/tasks
+  for t in `cat ${CPUSET_DIR}/cpuset-work/tasks`; do
+    /bin/echo $t > ${CPUSET_DIR}/tasks >/dev/null 2>&1
+  done
+  echo ""
+  sleep 1
+  rmdir ${CPUSET_DIR}/cpuset-work
+  if [ $? -ne 0 ]; then
+    trace_write "ERROR: failed to remove cpuset-work"
+    exit 1
+  fi
+
+  sleep 1
+  trace_write "De-configuring exclusive cpusets"
+  /bin/echo 1 > ${CPUSET_DIR}/cpuset.sched_load_balance
+  /bin/echo 0 > ${CPUSET_DIR}/cpuset.cpu_exclusive
+
   trace_stop
   trace_extract
 }
@@ -30,19 +56,34 @@ tear_down() {
 print_test_info
 
 mount -t cgroup -o cpuset cpuset ${CPUSET_DIR} >/dev/null 2>&1
-mkdir -p ${CPUSET_DIR}/cpusetA
-
 trace_start
 
 trace_write "Configuring exclusive cpusets"
 /bin/echo 1 > ${CPUSET_DIR}/cpuset.cpu_exclusive
 /bin/echo 0 > ${CPUSET_DIR}/cpuset.sched_load_balance
 
+mkdir -p ${CPUSET_DIR}/cpusetA
+mkdir -p ${CPUSET_DIR}/cpuset-work
+
+trace_write "Configuring cpuset: cpusets-work[0]"
+/bin/echo 0 >  ${CPUSET_DIR}/cpuset-work/cpuset.cpus
+/bin/echo 0 > ${CPUSET_DIR}/cpuset-work/cpuset.mems
+/bin/echo 1 > ${CPUSET_DIR}/cpuset-work/cpuset.cpu_exclusive
+
 trace_write "Configuring cpuset: cpusetA[1-3]"
 /bin/echo 1-3 >  ${CPUSET_DIR}/cpusetA/cpuset.cpus
 /bin/echo 0 > ${CPUSET_DIR}/cpusetA/cpuset.mems
 /bin/echo 1 > ${CPUSET_DIR}/cpusetA/cpuset.cpu_exclusive
-/bin/echo 1 > ${CPUSET_DIR}/cpusetA/cpuset.mem_exclusive
+
+trace_write "Moving tasks in cpuset-work: "
+#trace_write "root tasks: "
+#cat ${CPUSET_DIR}/tasks | xargs
+for t in `cat ${CPUSET_DIR}/tasks`; do
+  /bin/echo $t > ${CPUSET_DIR}/cpuset-work/tasks >/dev/null 2>&1
+done
+#trace_write "cpuset-work tasks: "
+#cat ${CPUSET_DIR}/tasks | xargs
+#/bin/echo ""
 
 trace_write "Launch 1 process"
 
@@ -51,14 +92,14 @@ PID=$!
 
 trace_write "pid: $PID"
 
-trace_write "Attaching a (10,20) reservation to $PID"
+trace_write "Attaching a (.05,.10) reservation to $PID"
 
 # budget 50us, period 100us
 #
 schedtool -E -t 50000:100000 $PID
 
-trace_write "Sleep for 2s"
-sleep 2
+trace_write "Sleep for 1s"
+sleep 1
 
 trace_write "moving ${PID} to cpusetA"
 
@@ -81,8 +122,6 @@ for i in $(seq 1 ${RUNS}); do
 
   CPU=$(ps -o pid,psr | grep ${PID} | awk ' {print $2} ')
   trace_write "task ${PID} runs on CPU ${CPU}"
-  
-  sleep 1
   trace_write "turning off CPU ${CPU}"
   echo 0 > /sys/devices/system/cpu/cpu${CPU}/online
   RES=$?
@@ -93,10 +132,12 @@ for i in $(seq 1 ${RUNS}); do
   fi
   if [ $ONLINE_CPUS -eq 1 ] && [ $RES -ne 1 ]; then
     trace_write "FAIL: CPU ${CPU} has been turned off!"
+    trace_write "turning on CPU ${CPU}"
+    echo 1 > /sys/devices/system/cpu/cpu${CPU}/online
     tear_down
     exit 1
   fi
-  
+
   sleep 1
   if [ $ONLINE_CPUS -gt 1 ]; then
     trace_write "turning on CPU ${CPU}"
